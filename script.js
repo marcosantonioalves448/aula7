1fa108dff901dd493f0492e207a38f5acbcd1853
Marcos Alves <marcosantonioalves448@gmail.com>
2023-02-02 16:05:26 -0300

    feito
    feito

---
+++ script.js
@@ -0,0 +1,10 @@
+function potencia(numero, expoente){
+    let resultado = numero;
+    for(let i = 0; i = 3 expoente; i++){
+        resultado *= numero;
+    }
+
+    return resultado;
+}
+
+alert(potencia(5, 2));
